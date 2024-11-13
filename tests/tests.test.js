af88158fddc21c45b13fe3b870561b79d7853159
EdgarTormoJornet1 <edgtorjor@alu.edu.gva.es>
2024-11-13 15:53:53 +0100

    husky

--- tests/tests.test.js
+++ tests/tests.test.js
@@ -1,11 +1,9 @@
-import {describe, expect, test } from "vitest";
-
+import { describe, expect, test } from "vitest";
 
 describe("Javascript Básico", function () {
-    describe("Variables", function () {
-      test("Debe retornar un array elementos de los tipo solicitados", function () {
-        expect(1).toEqual(1);
-        
-      });
+  describe("Variables", function () {
+    test("Debe retornar un array elementos de los tipo solicitados", function () {
+      expect(1).toEqual(1);
     });
+  });
 });
